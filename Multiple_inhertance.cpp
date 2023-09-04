69e53ef0fa9acf1707ef66f8217c47584a05ec3b
Ashok <68994450+AshokDon@users.noreply.github.com>
2023-09-04 10:25:39 +0530

    Create Multiple_inhertance.cpp

---
+++ Multiple_inhertance.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ll long long
+#define pb push_back
+using namespace std;
+
+void TakeInput(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+//Multiple inhertance // 
+class Father{
+	 public:
+	 	
+};
+class Mother  {
+public:
+	
+};
+class Child : public Father, public Mother{
+
+};
+int main() {
+    // your code goes here
+    fast_io;
+    TakeInput();
+    Student Ashok;
+    
+
+
+}
